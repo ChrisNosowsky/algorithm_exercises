4bb7f6eb41561dbabf2bc2a11b775b081b4f479f
hz5q9k <christopher.nosowsky@gm.com>
2020-08-05 16:24:06 -0400

    bubble sort

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,25 @@
+import math.h;
+
+
+class Solution {
+    void bubblesort(int array[]) {
+        boolean isSorted = false;
+        int lastUnsorted = array.length - 1;
+        while(!isSorted) {
+            isSorted = true;
+            for(int i=0; i < lastUnsorted; i++) {
+                if (array[i] > array[i+1]) {
+                    swap(array, i, i + 1);
+                    isSorted = false;
+                }
+            }
+            lastUnsorted--;
+        }
+    }
+
+    void swap(int array[], int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
