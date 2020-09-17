790d9f6c60966cc3f7cf415771ef011e1d98ffc7
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-16 23:56:31 -0400

    remove duplicates from array algorithm

---
+++ Arrays_and_Strings/IsUnique/removeDuplicates.py
@@ -0,0 +1,14 @@
+def remove_duplicates(arr):
+    master = dict()
+    org = [o for o in arr]
+    minus = 0
+    for ele in range(len(org)):
+        if org[ele] in master:
+            arr.pop(ele-minus)
+            minus += 1
+        else:
+            master[org[ele]] = 1
+    return arr
+
+array_ex = [10, 5, 7, 3, 20, 190, 190, 190, 190, 10, 7, 3, 6, 23, 7]
+print(remove_duplicates(array_ex))
